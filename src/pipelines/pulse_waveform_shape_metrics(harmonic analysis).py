5655c591aaa3c3cd4c24b9ddf4541adc3590bfe8
chloe <chloe.paquet1901@gmail.com>
2026-02-10 16:24:25 +0100

    new metrics

--- src/pipelines/pulse_waveform_shape_metrics(harmonic analysis).py
+++ src/pipelines/pulse_waveform_shape_metrics(harmonic analysis).py
@@ -25,78 +25,78 @@ class ArterialHarmonicAnalysis(ProcessPipeline):
         v_ds = np.asarray(h5file[self.v])
         t_ds = np.asarray(h5file[self.T])
         
+
+        
         N=len(v_ds[:,0])
         nb_harmonic=10
         Xn=[]
         
         CV_1=[]
         CV_2=[]
-        Vn_tot=[]
         
+        Vn_tot=[]
         sigma_phase_tot=[]
         v_hat=[]
         CF_tot=[]
         D_alpha=[]
-        V0=[]
+        
         FVTI=[]
         IVTI=[]
-        max=[]
+        AVTI_tot=[]
         
         
         alpha=0.5 #that needs to be specified
         for k in range(len(v_ds[0])):
-            
+            T=t_ds[0][k]
             fft_vals=np.fft.fft(v_ds[:,k])/N
             limit=nb_harmonic+1
-            Vn=fft_vals[1:limit]
-            V1=Vn[0]
+            Vn=fft_vals[:limit]
+            V1=Vn[1]
             Xn_k=Vn/V1
             Xn.append(Xn_k)
             Vn_tot.append(Vn)
             
-            omega0= (2 * np.pi)/t_ds[0][k]
-            V0.append(np.mean(v_ds[:,k]))
+            omega0= (2 * np.pi)/T
+            t=np.linspace(0,T,N)
             
-            v_hat_k=np.zeros_like(t_ds[0][k])
+            v_hat_k=np.real(Vn[0])*np.ones_like(t)
             
-            for n, v_coeff in enumerate (Vn,1):
-                h=n*omega0*2*(v_coeff*np.exp(1j*n*omega0*t_ds[0][k])).real
+            for i in range (1,limit):
+                h=2*(Vn[i]*np.exp(1j*i*omega0*t)).real
                 v_hat_k+=h
-            v_hat_V0=v_hat_k+V0[k]
-            v_hat.append(v_hat_V0)
-
-            v_hat_np=np.asarray(v_hat)
-            diff=v_hat_np.T[k]-v_ds[:,k]
-            max.append(np.maximum(diff, 0))
-            max_np=np.asarray(max)
+            v_hat.append(v_hat_k)
 
             RMST=np.sqrt(np.mean(v_hat[k]**2)) 
-            CF=np.max(v_hat)/RMST
+            CF=np.max(v_hat_k)/RMST
             CF_tot.append(CF)
            
-            amp1=np.abs(np.asarray(Vn_tot)[:,0])
-            amp2=np.abs(np.asarray(Vn_tot)[:,1])
+            amp1=np.abs(np.asarray(Vn_tot)[:,1])
+            amp2=np.abs(np.asarray(Vn_tot)[:,2])
 
             CV_1.append(np.std(amp1)/np.mean(amp1))
             CV_2.append(np.std(amp2)/np.mean(amp2))
 
-            phi1=np.angle(np.asarray(Xn)[:,0])
-            phi2=np.angle(np.asarray(Xn)[:,1]) 
+            phi1=np.angle(np.asarray(Xn)[:,1])
+            phi2=np.angle(np.asarray(Xn)[:,2]) 
 
             diff_phase=phi2-2*phi1
             diff_phase_wrap=(diff_phase+np.pi)%(2*np.pi)-np.pi
             sigma_phase=np.std(diff_phase_wrap)
             sigma_phase_tot.append(sigma_phase)
 
-            seuil=V0[k]+alpha*np.std(v_hat[k])
-            condition= v_hat[k] > seuil 
+            seuil=Vn[0]+alpha*np.std(v_hat)
+            condition= v_hat > seuil 
             D_alpha.append(np.mean(condition))
 
-                   
-            moitie = len(max) // 2
-            d1 = np.sum(max_np.T[k][:moitie])
-            d2 = np.sum(max_np.T[k][moitie:])
-            AVTI=np.sum(max_np.T[k])
+            v_base=np.min(v_hat[k])
+            max=np.maximum(v_hat[k]-v_base, 0)
+            
+            dt=t[1]-t[0]
+            moitie = len(t) // 2
+            d1 = np.sum(max[:moitie])*dt
+            d2 = np.sum(max[moitie:])*dt
+            AVTI=np.sum(max)*dt
+            AVTI_tot.append(AVTI)
             FVTI.append(d1/AVTI)
             IVTI.append((d1-d2)/(d1+d2))
 
@@ -108,56 +108,56 @@ class ArterialHarmonicAnalysis(ProcessPipeline):
                     "unit": [""],
                 },
             ),
-            "Beat to beat amplitude stability (n=1)": with_attrs(
+            "AVTI": with_attrs(
+                np.asarray(AVTI_tot),
+                {
+                    "unit": [""],
+                },
+            ),
+            "CV1 : Beat to beat amplitude stability (n=1)": with_attrs(
                 np.asarray(CV_1),
                 {
                     "unit": [""],
                 },
             ),
-            "Beat to beat amplitude stability (n=2)": with_attrs(
+            "CV2 : Beat to beat amplitude stability (n=2)": with_attrs(
                 np.asarray(CV_2),
                 {
                     "unit": [""],
                 },
             ),
-            "Beat to beat phase coupling stability (n=2)": with_attrs(
+            "sigma : Beat to beat phase coupling stability (n=2)": with_attrs(
                 np.asarray(sigma_phase_tot),
                 {
                     "unit": [""],
                 },
             ),
-             "Band limited waveform (definition) : v_hat": with_attrs(
+             "v_hat : Band limited waveform (definition)": with_attrs(
                 np.asarray(v_hat),
                 {
                     "unit": [""],
                 },
             ), 
-            "Band limited crest factor : CF": with_attrs(
+            "CF : Band limited crest factor ": with_attrs(
                 np.asarray(CF_tot),
                 {
                     "unit": [""],
                 },
             ), 
-            "Effective Duty cycle : D_alpha": with_attrs(
+            " D_alpha : Effective Duty cycle ": with_attrs(
                 np.asarray(D_alpha),
                 {
                     "unit": [""],
                 },
-            ), 
-            "V0": with_attrs(
-                np.asarray(V0),
-                {
-                    "unit": [""],
-                },
-            ), 
-            "Normalised first half fraction : FVTI": with_attrs(
+            ),
+            "FVTI : Normalised first half fraction ": with_attrs(
                 np.asarray(FVTI),
                 {
                     "unit": [""],
                 },
             ), 
             
-            "VTI asymmetry index : IVTI": with_attrs(
+            "IVTI : VTI asymmetry index ": with_attrs(
                 np.asarray(IVTI),
                 {
                     "unit": [""],
