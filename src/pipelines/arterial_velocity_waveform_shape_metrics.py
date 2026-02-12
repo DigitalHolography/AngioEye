99db1e3b8b37b1e09ccc86a7f283e13fcf119738
gregoire <gregoire.buisson@telecom-paris.fr>
2026-02-12 12:18:01 +0100

    clean R_VTI tau_M1 and RI both global and bandlimited metrics pipeline code

--- src/pipelines/arterial_velocity_waveform_shape_metrics.py
+++ src/pipelines/arterial_velocity_waveform_shape_metrics.py
@@ -3,7 +3,7 @@ import numpy as np
 from .core.base import ProcessPipeline, ProcessResult, registerPipeline, with_attrs
 
 
-@registerPipeline(name="arterialformshape")
+@registerPipeline(name="arterial_waveform_shape_metrics")
 class ArterialExample(ProcessPipeline):
     """
     Tutorial pipeline showing the full surface area of a pipeline:
@@ -16,61 +16,106 @@ class ArterialExample(ProcessPipeline):
     """
 
     description = "Tutorial: metrics + artifacts + dataset attrs + file/pipeline attrs."
-    v_raw = "/Artery/VelocityPerBeat/VelocitySignalPerBeat/value"
-    v = "/Artery/VelocityPerBeat/VelocitySignalPerBeatBandLimited/value"
+    v_raw_input = "/Artery/VelocityPerBeat/VelocitySignalPerBeat/value"
+    v_bandlimited_input = (
+        "/Artery/VelocityPerBeat/VelocitySignalPerBeatBandLimited/value"
+    )
     T = "/Artery/VelocityPerBeat/beatPeriodSeconds/value"
-    vmax = "/Artery/VelocityPerBeat/VmaxPerBeatBandLimited/value"
-    vmin = "/Artery/VelocityPerBeat/VminPerBeatBandLimited/value"
+    v_bandlimited_max_input = "/Artery/VelocityPerBeat/VmaxPerBeatBandLimited/value"
+    v_bandlimited_min_input = "/Artery/VelocityPerBeat/VminPerBeatBandLimited/value"
 
     def run(self, h5file) -> ProcessResult:
-        vraw_ds_temp = np.asarray(h5file[self.v_raw])
-        vraw_ds = np.maximum(vraw_ds_temp, 0)
-        v_ds_temp = np.asarray(h5file[self.v])
-        v_ds = np.maximum(v_ds_temp, 0)
-        t_ds = np.asarray(h5file[self.T])
-        V_max = np.asarray(h5file[self.vmax])
-        V_min = np.asarray(h5file[self.vmin])
-        TMI_raw = []
-        RTVI = []
-        RTVI_raw = []
-        for k in range(len(t_ds[0])):
-            D1_raw = np.sum(vraw_ds.T[k][:31])
-            D2_raw = np.sum(vraw_ds.T[k][32:])
-            D1 = np.sum(v_ds.T[k][:31])
-            D2 = np.sum(v_ds.T[k][32:])
-            RTVI.append(D1 / (D2 + 10 ** (-12)))
-            RTVI_raw.append(D1_raw / (D2_raw + 10 ** (-12)))
-            M_0 = np.sum(vraw_ds.T[k])
-            M_1 = 0
-            for i in range(len(vraw_ds.T[k])):
-                M_1 += vraw_ds[i][k] * i * t_ds[0][k] / 64
-            TM1 = M_1 / (t_ds[0][k] * M_0)
-            TMI_raw.append(TM1)
-        TMI = []
-        for k in range(len(t_ds[0])):
-            M_0 = np.sum(v_ds.T[k])
-            M_1 = 0
-            for i in range(len(vraw_ds.T[k])):
-                M_1 += v_ds[i][k] * i * t_ds[0][k] / 64
-            TM1 = M_1 / (t_ds[0][k] * M_0)
-            TMI.append(TM1)
-        RI = []
-        for i in range(len(V_max[0])):
-            RI_temp = 1 - (V_min[0][i] / V_max[0][i])
-            RI.append(RI_temp)
+        v_raw = np.asarray(h5file[self.v_raw_input])
+        v_raw = np.maximum(v_raw, 0)
+        v_bandlimited = np.asarray(h5file[self.v_bandlimited_input])
+        v_bandlimited = np.maximum(v_bandlimited, 0)
+        T_ds = np.asarray(h5file[self.T])
+        v_bandlimited_max = np.asarray(h5file[self.v_bandlimited_max_input])
+        v_bandlimited_max = np.maximum(v_bandlimited_max, 0)
+        v_bandlimited_min = np.asarray(h5file[self.v_bandlimited_min_input])
+        v_bandlimited_min = np.maximum(v_bandlimited_min, 0)
+        tau_M1_raw = []
+        tau_M1_over_T_raw = []
+        tau_M1_bandlimited = []
+        tau_M1_over_T_bandlimited = []
+
+        R_VTI_bandlimited = []
+        R_VTI_raw = []
+
+        RI_bandlimited = []
         RI_raw = []
-        for i in range(len(V_max[0])):
-            RI_temp = 1 - (np.min(vraw_ds.T[i]) / np.max(vraw_ds.T[i]))
-            RI_raw.append(RI_temp)
+
+        ratio_systole_diastole_R_VTI = 0.5
+
+        for beat_idx in range(len(T_ds[0])):
+            t = T_ds[0][beat_idx] / len(v_raw.T[beat_idx])
+            D1_raw = np.sum(
+                v_raw.T[beat_idx][
+                    : int(np.ceil(len(v_raw.T[0]) * ratio_systole_diastole_R_VTI))
+                ]
+            )
+            D2_raw = np.sum(
+                v_raw.T[beat_idx][
+                    int(np.ceil(len(v_raw.T[0]) * ratio_systole_diastole_R_VTI)) :
+                ]
+            )
+            D1_bandlimited = np.sum(
+                v_bandlimited.T[beat_idx][
+                    : int(
+                        np.ceil(len(v_bandlimited.T[0]) * ratio_systole_diastole_R_VTI)
+                    )
+                ]
+            )
+            D2_bandlimited = np.sum(
+                v_bandlimited.T[beat_idx][
+                    int(
+                        np.ceil(len(v_bandlimited.T[0]) * ratio_systole_diastole_R_VTI)
+                    ) :
+                ]
+            )
+            R_VTI_bandlimited.append(D2_bandlimited / (D1_bandlimited + 10 ** (-12)))
+            R_VTI_raw.append(D2_raw / (D1_raw + 10 ** (-12)))
+            M_0 = np.sum(v_raw.T[beat_idx])
+            M_1 = 0
+            for time_idx in range(len(v_raw.T[beat_idx])):
+                M_1 += v_raw[time_idx][beat_idx] * time_idx * t
+            TM1 = M_1 / M_0
+            tau_M1_raw.append(TM1)
+            tau_M1_over_T_raw.append(TM1 / T_ds[0][beat_idx])
+
+        for beat_idx in range(len(T_ds[0])):
+            t = T_ds[0][beat_idx] / len(v_raw.T[beat_idx])
+            M_0 = np.sum(v_bandlimited.T[beat_idx])
+            M_1 = 0
+            for time_idx in range(len(v_raw.T[beat_idx])):
+                M_1 += v_bandlimited[time_idx][beat_idx] * time_idx * t
+            TM1 = M_1 / M_0
+            tau_M1_bandlimited.append(TM1)
+            tau_M1_over_T_bandlimited.append(TM1 / T_ds[0][beat_idx])
+
+        for beat_idx in range(len(v_bandlimited_max[0])):
+            RI_bandlimited_temp = 1 - (
+                v_bandlimited_min[0][beat_idx] / v_bandlimited_max[0][beat_idx]
+            )
+            RI_bandlimited.append(RI_bandlimited_temp)
+
+        for beat_idx in range(len(v_bandlimited_max[0])):
+            RI_raw_temp = 1 - (np.min(v_raw.T[beat_idx]) / np.max(v_raw.T[beat_idx]))
+            RI_raw.append(RI_raw_temp)
 
         # Metrics are the main numerical outputs; each key becomes a dataset under /pipelines/<name>/metrics.
         metrics = {
-            "TMI_raw": with_attrs(np.asarray(TMI_raw), {"unit": [""]}),
-            "TMI": np.asarray(TMI),
-            "RI": np.asarray(RI),
+            "tau_M1_raw": with_attrs(np.asarray(tau_M1_raw), {"unit": [""]}),
+            "tau_M1_bandlimited": np.asarray(tau_M1_bandlimited),
+            "tau_M1_over_T_raw": with_attrs(
+                np.asarray(tau_M1_over_T_raw), {"unit": [""]}
+            ),
+            "tau_M1_over_T_bandlimited": np.asarray(tau_M1_over_T_bandlimited),
+            "RI_bandlimited": np.asarray(RI_bandlimited),
             "RI_raw": np.asarray(RI_raw),
-            "RTVI": np.asarray(RTVI),
-            "RTVI_raw": np.asarray(RTVI_raw),
+            "R_VTI_bandlimited": np.asarray(R_VTI_bandlimited),
+            "R_VTI_raw": np.asarray(R_VTI_raw),
+            "ratio_systole_diastole_R_VTI": np.asarray(ratio_systole_diastole_R_VTI),
         }
 
         # Artifacts can store non-metric outputs (strings, paths, etc.).
