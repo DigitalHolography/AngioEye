e50908f4ed088e69e5247d6fb84c86624d2c2750
Witty077 <rainingsomedays@gmail.com>
2026-04-24 14:45:36 +0200

    built the input interface

---
+++ src/pipelines/core/dev_womersley_modeling/womersley_modeling.py
@@ -0,0 +1,20 @@
+# import numpy as np
+
+# from ..base import ProcessPipeline, ProcessResult, registerPipeline, with_attrs
+
+
+# @registerPipeline(name="WomersleyModeling")
+# class WomersleyModeling(ProcessPipeline):
+
+
+#     description = (
+#         "TODO: Add description of the WomersleyModeling pipeline here."
+#     )
+
+
+#     v_profile_segment_onebeat = (
+#         "/Artery/CrossSections/VelocityProfilesSegInterpOneBeat/value"
+#     )
+
+# @classmethod
+# to transfer the input to module TODO
