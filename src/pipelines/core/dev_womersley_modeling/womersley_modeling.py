941eb28762517cbc6f914b6ac592d1bd291763c7
Witty077 <rainingsomedays@gmail.com>
2026-04-28 13:45:40 +0200

    registered womersleymodling

--- src/pipelines/core/dev_womersley_modeling/womersley_modeling.py
+++ src/pipelines/core/dev_womersley_modeling/womersley_modeling.py
@@ -1,20 +1,36 @@
-# import numpy as np
+import h5py
 
-# from ..base import ProcessPipeline, ProcessResult, registerPipeline, with_attrs
-
-
-# @registerPipeline(name="WomersleyModeling")
-# class WomersleyModeling(ProcessPipeline):
+from ..base import ProcessPipeline, ProcessResult, registerPipeline
 
 
-#     description = (
-#         "TODO: Add description of the WomersleyModeling pipeline here."
-#     )
+@registerPipeline(name="WomersleyModeling")
+class WomersleyModeling(ProcessPipeline):
+    description = "TODO."
 
+    v_profile = "/Artery/CrossSections/VelocityProfilesSegInterpOneBeat/value"
 
-#     v_profile_segment_onebeat = (
-#         "/Artery/CrossSections/VelocityProfilesSegInterpOneBeat/value"
-#     )
+    def run(self, h5file: h5py.File) -> ProcessResult:
+        """
+        Executes the Womersley Modeling pipeline.
+        """
 
-# @classmethod
-# to transfer the input to module TODO
+        obj = h5file[self.v_profile]
+
+        if not isinstance(obj, h5py.Dataset):
+            raise ValueError(
+                f"The path '{self.v_profile}' does not point to a valid dataset in the HDF5 file."
+            )
+        v_profile = obj[:]
+
+        print(f"=== Data Structure for '{self.v_profile}' ===")
+        print(f"HDF5 Object Type: {type(obj)}")
+        print(f"Data Shape: {v_profile.shape}")
+        print(f"Data Dtype: {v_profile.dtype}")
+
+        if obj.attrs:
+            print("Attributes:")
+            for key, val in obj.attrs.items():
+                print(f"  - {key}: {val}")
+        print("=================================================================")
+
+        return ProcessResult(metrics={}, attrs={})
