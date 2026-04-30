557bcbceeb90ac58dcd90134a5631b85f8e81d1d
Witty077 <rainingsomedays@gmail.com>
2026-04-30 16:08:37 +0200

    fixed minor issue

--- src/pipelines/dev_v_profile_modeling/v_profile_meas_extraction.py
+++ src/pipelines/dev_v_profile_modeling/v_profile_meas_extraction.py
@@ -14,7 +14,7 @@ def preprocess_and_interpolate(num_interp_points, v_segment_onebeat):
     min_idx = valid_indices[0]
     max_idx = valid_indices[-1]
 
-    v_valid = v_segment_onebeat[min_idx : max_idx + 1]
+    v_valid = v_segment_onebeat[min_idx : max_idx + 1].copy()
     v_valid[0], v_valid[-1] = 0.0, 0.0
     x_valid = np.arange(len(v_valid))
     x_interp = np.linspace(0, len(v_valid) - 1, num=num_interp_points)
