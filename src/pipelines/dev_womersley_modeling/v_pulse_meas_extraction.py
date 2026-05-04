6ff74043f63f227272ab4969d986a9bef81244bd
Witty077 <rainingsomedays@gmail.com>
2026-05-04 12:47:11 +0200

    created velocity pulse per segment extraction

--- src/pipelines/dev_womersley_modeling/v_pulse_meas_extraction.py
+++ src/pipelines/dev_womersley_modeling/v_pulse_meas_extraction.py
@@ -2,20 +2,17 @@ import numpy as np
 from scipy.interpolate import interp1d
 
 
-def preprocess_and_interpolate(num_interp_points, v_profile):
-    valid_mask = ~np.isnan(v_profile)
+def preprocess_and_interpolate(num_interp_points, v_pulse):
+    valid_mask = ~np.isnan(v_pulse)
     valid_indices = np.where(valid_mask)[0]
-    valid_count = np.sum(valid_mask)
 
-    if valid_count <= 8:
-        print(f"Warning: Only {valid_count} valid points found. Skipping...")
-        return np.zeros(num_interp_points)
+    if valid_indices.size == 0:
+        return np.zeros(len(v_pulse))
 
     min_idx = valid_indices[0]
     max_idx = valid_indices[-1]
 
-    v_valid = v_profile[min_idx : max_idx + 1].copy()
-    v_valid[0], v_valid[-1] = 0.0, 0.0
+    v_valid = v_pulse[min_idx : max_idx + 1].copy()
     x_valid = np.arange(len(v_valid))
     x_interp = np.linspace(0, len(v_valid) - 1, num=num_interp_points)
 
@@ -31,32 +28,30 @@ def preprocess_and_interpolate(num_interp_points, v_profile):
     return np.asanyarray(v_interp)
 
 
-def extract_v_profile_meas(dataset, num_interp_points, n_harmonic):
+def extract_v_pulse_meas(dataset, num_interp_points, n_harmonic):
     # Expected shape: (n_t, n_x, n_branches, n_radii) -> (128, 33, 14, 10)
     n_t, n_x, n_branches, n_radii = dataset.shape
-    v_profile_fft = np.zeros(
-        (n_t, num_interp_points, n_branches, n_radii), dtype=complex
-    )
-    v_profile_meas = np.zeros(
-        (n_t, num_interp_points, n_branches, n_radii), dtype=complex
+    v_pulse_fft = np.zeros((num_interp_points, n_x, n_branches, n_radii), dtype=complex)
+    v_pulse_meas = np.zeros(
+        (num_interp_points, n_x, n_branches, n_radii), dtype=complex
     )
 
     for branch_idx in range(n_branches):
         for radii_idx in range(n_radii):
-            for t_idx in range(n_t):
-                v_profile = np.asarray(dataset[t_idx, :, branch_idx, radii_idx])
+            for x_idx in range(n_x):
+                v_pulse = np.asarray(dataset[:, x_idx, branch_idx, radii_idx])
 
                 v_interp = preprocess_and_interpolate(
                     num_interp_points=num_interp_points,
-                    v_profile=v_profile,
+                    v_pulse=v_pulse,
                 )
 
                 v_fft = np.fft.fft(np.asarray(v_interp), n=num_interp_points)
-                v_profile_fft[t_idx, :, branch_idx, radii_idx] = v_fft
+                v_pulse_fft[:, x_idx, branch_idx, radii_idx] = v_fft
 
                 v_meas = np.zeros_like(v_fft)
                 v_meas[1] = v_fft[n_harmonic]
                 v_meas[-1] = v_fft[-n_harmonic]
-                v_profile_meas[t_idx, :, branch_idx, radii_idx] = np.fft.ifft(v_meas)
+                v_pulse_meas[:, x_idx, branch_idx, radii_idx] = np.fft.ifft(v_meas)
 
-    return v_profile_fft, v_profile_meas
+    return v_pulse_fft, v_pulse_meas
---
+++ src/pipelines/womersley_modeling.py
@@ -0,0 +1,39 @@
+import h5py
+import numpy as np
+
+from .core.base import ProcessPipeline, ProcessResult, registerPipeline
+from .dev_womersley_modeling.v_pulse_meas_extraction import (
+    extract_v_pulse_meas,
+)
+
+R0 = 0.05  # Vessel radius in mm
+num_interp_points = 128  # Number of temporal points for interpolation
+n_harmonic = 1
+
+
+@registerPipeline(name="WomersleyModeling")
+class WomersleyModeling(ProcessPipeline):
+    description = "Womersley Modeling Pipeline"
+
+    v_profile_path = "/Artery/CrossSections/VelocityProfilesSegInterpOneBeat/value"
+
+    def run(self, h5file: h5py.File) -> ProcessResult:
+        """
+        Executes the Womersley Modeling pipeline.
+        """
+        obj = h5file[self.v_profile_path]
+        if not isinstance(obj, h5py.Dataset):
+            raise ValueError(
+                f"Expected a dataset at {self.v_profile_path}, but found {type(obj)}"
+            )
+        dataset = obj[:]
+
+        v_pulse_fft, v_pulse_meas = extract_v_pulse_meas(
+            dataset=dataset, num_interp_points=num_interp_points, n_harmonic=n_harmonic
+        )
+
+        metrics: dict = {}
+        metrics["v_pulse_fft"] = np.asarray(v_pulse_fft)
+        metrics["v_pulse_meas"] = np.asarray(v_pulse_meas)
+
+        return ProcessResult(metrics=metrics)
